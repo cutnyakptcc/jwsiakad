d70b04cfd981320b0330421e63cb6ca993b7f27a
cutnyakptcc <cutnyakputroecendanalp3i@gmail.com>
2024-01-31 15:30:04 +0700

    login

--- jurusan.php
+++ jurusan.php
@@ -7,7 +7,7 @@ include_once ("cek_login.php");
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title>Data Kampus| Blank Page</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -35,12 +35,13 @@ include_once ("cek_login.php");
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>Data Jurusan</h1>
+ 
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
+              <li class="breadcrumb-item active">Data Jurusan</li>
             </ol>
           </div>
         </div>
@@ -65,7 +66,7 @@ include_once ("cek_login.php");
           </div>
         </div>
         <div class="card-body">
-          Data Jurusan
+          Start creating your amazing application!
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "wpcyk";
+
+$con = mysqli_connect($host,$username,$password,$database);
+
+
+?> 
